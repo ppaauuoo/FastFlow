302e205da4b16c04536ff27a8437e17bcc0dcdac
Vorakan S <vorakan.sumethsenee@wdc.com>
2026-01-27 09:39:46 +0700

    ci: tuning parameters, overkill folder

--- crop.bat
+++ crop.bat
@@ -1,2 +1,3 @@
 uv run circle_crop.py --input ./data/Pass --config ./configs/circle_crop.yaml
+uv run circle_crop.py --input ./data/Overkill --config ./configs/circle_crop.yaml
 uv run circle_crop.py --input ./data/Reject --config ./configs/circle_crop.yaml
